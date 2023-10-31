b04dcac93968129fb9a771c586f4ccea72084234
SergioNascimento07 <sergioricjr7@gmail.com>
2023-10-30 22:31:48 -0300

    feat: métricas criadas e prontas para ser utilizadas

---
+++ fastapi-app/prometheus-metrics.py
@@ -0,0 +1,26 @@
+from prometheus_client import Counter, CollectorRegistry, Summary, Gauge, pushadd_to_gateway
+
+registry = CollectorRegistry()
+
+status_http_counter = Counter(
+    'http_requests_total_by_code',
+    'total de respostas por status http',
+    ['http_code'],
+    registry=registry
+)
+
+http_request_duration = Summary(
+    'http_requests_duration_seconds',
+    'reponse time of request',
+    ['url_path', 'http_method'],
+    registry=registry
+)
+
+requests_in_progress = Gauge(
+    'request_in_progress_total',
+    'quantity of requests in progress',
+    registry=registry
+)
+
+def send_metrics():
+    pushadd_to_gateway('pushgateway:9091', job='fastapi-app', registry=registry)
