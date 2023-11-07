a37dbd59b098e0e3365cfd70be667381270e94ab
Sergio <sergioricjr7@gmail.com>
2023-11-07 14:10:11 +0000

    devocontainer

--- fastapi-app/main.py
+++ fastapi-app/main.py
@@ -4,19 +4,44 @@ import httpx
 from fastapi import FastAPI
 from  middleware.ObservabilityMiddleware import ObservabilityMiddleware
 from observability_config.log_config import logger
+from starlette_prometheus import metrics, PrometheusMiddleware
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+from opentelemetry import trace
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+# from main import app
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+
+resource = Resource.create({SERVICE_NAME: "fastapi-app"})
+trace.set_tracer_provider(TracerProvider(resource=resource))
+
+trace.get_tracer_provider().add_span_processor(
+    BatchSpanProcessor(OTLPSpanExporter(endpoint="http://nginx:80/tempo/v1/traces"))
+)
+
+tracer = trace.get_tracer(__name__)
+
+# FastAPIInstrumentor.instrument_app(app, tracer_provider=trace.get_tracer_provider())
+from opentelemetry.sdk.trace import TracerProvider
 
 app = FastAPI()
 
 app.add_middleware(ObservabilityMiddleware)
+app.add_middleware(PrometheusMiddleware)
+app.add_route("/metrics", metrics)
 
 @app.get("/")
 def welcome():
     logger.info('hello message was sent')
+    print("welcome")
     return {"message": "Hello, welcome to the application!"}
 
 @app.get("/random")
 def get_random_number():
     random_number = random.randint(0, 100)
+    print("random")
     logger.info('random number between 0 and 100 was calculated')
     return {"message": f"The random number was created", "number": random_number}
 
@@ -45,6 +70,7 @@ async def multiple_requests():
         logger.info('multiple requests were made successfully')
         return {"message": "Multiple requests are sent"}
 
+FastAPIInstrumentor.instrument_app(app, tracer_provider=trace.get_tracer_provider())
 
 if __name__ == "__main__":
     uvicorn.run(
