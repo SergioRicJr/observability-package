d49138578190ba94e18db77b9cde97430d94f956
Sergio <sergioricjr7@gmail.com>
2023-11-08 03:18:41 +0000

    feat(main): trace aplicado e funcional no arquivo principal, pegando span e trace id de forma automática

--- fastapi-app/main.py
+++ fastapi-app/main.py
@@ -5,26 +5,8 @@ from fastapi import FastAPI
 from  middleware.ObservabilityMiddleware import ObservabilityMiddleware
 from observability_config.log_config import logger
 from starlette_prometheus import metrics, PrometheusMiddleware
+from observability_config.trace_config import trace
 from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
-from opentelemetry import trace
-from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchSpanProcessor
-from opentelemetry.sdk.resources import SERVICE_NAME, Resource
-from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
-# from main import app
-from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
-
-resource = Resource.create({SERVICE_NAME: "fastapi-app"})
-trace.set_tracer_provider(TracerProvider(resource=resource))
-
-trace.get_tracer_provider().add_span_processor(
-    BatchSpanProcessor(OTLPSpanExporter(endpoint="http://nginx:80/tempo/v1/traces"))
-)
-
-tracer = trace.get_tracer(__name__)
-
-# FastAPIInstrumentor.instrument_app(app, tracer_provider=trace.get_tracer_provider())
-from opentelemetry.sdk.trace import TracerProvider
 
 app = FastAPI()
 
--- fastapi-app/observability_config/trace_config.py
+++ fastapi-app/observability_config/trace_config.py
@@ -1,18 +1,14 @@
-# from opentelemetry import trace
-# from opentelemetry.sdk.trace import TracerProvider
-# from opentelemetry.sdk.trace.export import BatchSpanProcessor
-# from opentelemetry.sdk.resources import SERVICE_NAME, Resource
-# from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
-# from main import app
-# from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+from opentelemetry import trace
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 
-# resource = Resource.create({SERVICE_NAME: "fastapi-app"})
-# trace.set_tracer_provider(TracerProvider(resource=resource))
+resource = Resource.create({SERVICE_NAME: "fastapi-app"})
+trace.set_tracer_provider(TracerProvider(resource=resource))
 
-# trace.get_tracer_provider().add_span_processor(
-#     BatchSpanProcessor(OTLPSpanExporter(endpoint="http://nginx:80/tempo/v1/traces"))
-# )
+trace.get_tracer_provider().add_span_processor(
+    BatchSpanProcessor(OTLPSpanExporter(endpoint="http://nginx:80/tempo/v1/traces"))
+)
 
-# tracer = trace.get_tracer(__name__)
-
-# FastAPIInstrumentor.instrument_app(app, tracer_provider=trace.get_tracer_provider())
+tracer = trace.get_tracer(__name__)
